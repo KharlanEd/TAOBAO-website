497732e4d43911e98fb3fa67873511eb743ac326
KharlanEd <eduardxarlan@gmail.com>
2023-07-09 15:36:42 +0300

    fix tailwind

--- components/Hero.js
+++ components/Hero.js
@@ -4,10 +4,11 @@
 
      return (
          <>
-           <div>
+           <div className="bg-gray-200 p-4">
                 <main>
-                    <h1>Хочеш купувати бренди дешевше ніж в Україні?
-                        Тоді тобі точно сюди !</h1>
+                    <h1 className="text-2xl font-bold text-gray-800" >Хочеш купувати бренди дешевше ніж в Україні?
+                         Тоді тобі точно сюди !</h1>
+                     <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mt-4 rounded">Нажми меня</button>
                 </main>
             </div>       
          </>
