875b6b69e3dfa5970640ab7e02e9290f40c951b0
KharlanEd <eduardxarlan@gmail.com>
2023-07-09 17:24:52 +0300

    fix

--- components/Hero.js
+++ components/Hero.js
@@ -4,14 +4,18 @@
 
      return (
          <>
-           <div className="bg-gray-200 p-4">
-                <main>
-                    <h1 className="text-2xl font-bold text-gray-800" >Хочеш купувати бренди дешевше ніж в Україні?
+        
+             <div className="bg-cover bg-center " style={{ backgroundImage: 'url("../public/bg-mary.jpg")' }}>
+             
+             <h1 className="text-title font-heading text-2xl font-bold " >Хочеш купувати бренди дешевше ніж в Україні?
                          Тоді тобі точно сюди !</h1>
-                     <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mt-4 rounded">Нажми меня</button>
-                </main>
-            </div>       
-         </>
+             
+             </div> 
+                 
+                    
+                    
+                
+            </>
      )
      
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,20 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
  ],
   theme: {
+    screens: {
+      sm: '375px',
+      md: '768px',
+      lg: '1280px',
+      
+    },
+    colors: {
+      'bg-container': '#251B37',
+      'title': '#FFCACA',
+    },
+    fontFamily: {
+        sans: ['Montserrat', 'sans-serif'],
+        heading: ['Philosopher', 'sans-serif'],
+      },
     extend: {},
   },
   plugins: [],
