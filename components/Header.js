59a153e2c9828a0d0922632122ed4542b7a831ab
KharlanEd <eduardxarlan@gmail.com>
2023-07-10 18:18:51 +0300

    Heder+footer

---
+++ components/Header.js
@@ -0,0 +1,13 @@
+
+
+export const Header = () => {
+  return (
+    <header className="bg-gray-800 text-white">
+      <nav className="flex justify-between items-center p-4">
+        <a href="#" className="text-white hover:text-gray-300">Обо мне</a>
+        <a href="#" className="text-white hover:text-gray-300">Купить</a>
+        <a href="#" className="text-white hover:text-gray-300">Отзывы</a>
+      </nav>
+    </header>
+  );
+};
