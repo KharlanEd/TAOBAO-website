fa9a4f5c61e425324008b7db29d805cb77a5ab44
KharlanEd <eduardxarlan@gmail.com>
2023-07-10 21:48:24 +0300

    header

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,30 @@
-
+import styled from '../styles/Hero.module.css'
 
 export const Header = () => {
   return (
-    <header className="bg-gray-800 text-white">
+    <header className="bg-gray-800  text-title-white">
       <nav className="flex justify-between items-center p-4">
-        <a href="#" className="text-white hover:text-gray-300">Обо мне</a>
-        <a href="#" className="text-white hover:text-gray-300">Купить</a>
-        <a href="#" className="text-white hover:text-gray-300">Отзывы</a>
+             <a className={styled.heder_link}>
+        <svg width="150px" height="50px" viewBox="0 0 180 60" className={styled.svg}>
+          <polyline points="179,1 179,59 1,59 1,1 179,1" class="bg-line" />
+          <polyline points="179,1 179,59 1,59 1,1 179,1" class="hl-line" />
+        </svg>
+        <span>Про мене</span>
+      </a>
+            <a className={styled.heder_link}>
+        <svg width="150px" height="50px" viewBox="0 0 180 60" className={styled.svg}>
+          <polyline points="179,1 179,59 1,59 1,1 179,1" class="bg-line" />
+          <polyline points="179,1 179,59 1,59 1,1 179,1" class="hl-line" />
+        </svg>
+        <span>Купить</span>
+      </a>
+              <a className={styled.heder_link}>
+        <svg width="150px" height="50px" viewBox="0 0 180 60" className={styled.svg}>
+          <polyline points="179,1 179,59 1,59 1,1 179,1" class="bg-line" />
+          <polyline points="179,1 179,59 1,59 1,1 179,1" class="hl-line" />
+        </svg>
+        <span>Відгуки</span>
+      </a>
       </nav>
     </header>
   );
--- components/Layout.js
+++ components/Layout.js
@@ -4,7 +4,7 @@ import { Header } from "./Header";
 
 export const Layout = ({ children }) => {
   return (
-    <div className="container bg-bg-container mx-auto px-4 sm:px-4 md:px-4 lg:px-4 ">
+    <div className="container  mx-auto px-4 sm:px-4 md:px-4 lg:px-4 ">
       <Header/>
       {children}
       <Footer/>
