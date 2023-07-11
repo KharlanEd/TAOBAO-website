971e1a10b4c5693e0814edadcf8616916706be2c
KharlanEd <eduardxarlan@gmail.com>
2023-07-11 18:28:30 +0300

    fix

--- components/Hero.js
+++ components/Hero.js
@@ -8,11 +8,13 @@ import Image from "next/image"
          <>
         
              <div className={styled.bg_wraper}>
+                 <div className={styled.text_animation}>
                  <h1 className={styled.title}>
                     Хочеш купувати бренди дешевше ніж в Україні? Тоді тобі точно сюди !
-                 </h1>  
-                 <p className={styled.text}>На звʼязку Марія! Вже більше року я працюю з Китаєм та маю власний магазин жіночого одягу. 
-                </p>
+                     </h1> 
+                 </div>    
+                 <h3 className={styled.text}>На звʼязку Марія! Вже більше року я працюю з Китаєм та маю власний магазин жіночого одягу. 
+                </h3>
 
              </div>
               
