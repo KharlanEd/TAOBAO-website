5a9b334c9e1412def105c46e51b7a9966f0da093
KharlanEd <eduardxarlan@gmail.com>
2023-07-21 16:47:23 +0300

    HeroDesktop

---
+++ components/HeroDesktop.js
@@ -0,0 +1,14 @@
+import Image from "next/image"
+
+export const HeroDesktop = () => {
+    
+
+    return (
+        
+        <section>
+            <div>
+                 <Image src="/images/product/mary_hero.jpg" width={342} height={530} alt="mary" />
+            </div>
+        </section>
+    )
+}
