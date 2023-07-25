1098ff34c86d6447add13a9b6caeb6e2eb3d543c
KharlanEd <eduardxarlan@gmail.com>
2023-07-25 22:06:28 +0300

    fix bag

--- components/Reviews.js
+++ components/Reviews.js
@@ -10,13 +10,13 @@ import "swiper/css/pagination";
 import "swiper/css/autoplay";
 
 const reviews = [
-  { id: 1, photoFilename: "Rectangle5.jpg" },
-  { id: 2, photoFilename: "Rectangle6.jpg" },
-  { id: 3, photoFilename: "Rectangle31.jpg" },
-  { id: 4, photoFilename: "Rectangle1.jpg" },
+  { id: 1, photoFilename: "IMG_3633.PNG" },
+  { id: 2, photoFilename: "IMG_3634.PNG" },
+  { id: 3, photoFilename: "IMG_3636.PNG" },
+  { id: 4, photoFilename: "IMG_3637.PNG" },
   { id: 5, photoFilename: "Rectangle2.jpg" },
   { id: 6, photoFilename: "Rectangle3.jpg" },
-  { id: 7, photoFilename: "IMG_2953.jpg" },
+  { id: 7, photoFilename: "IMG_3650.PNG" },
   { id: 8, photoFilename: "IMG_2954.jpg" },
 ];
 
